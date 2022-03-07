ea98437bbb8d1c85e89b2cb7d7ea03303a88ce40
maheshwari-mohit <mohit26.maheshwari@gmail.com>
2022-03-07 20:15:37 +0530

    Code for importing custom role, updated readme, and config

--- README.md
+++ README.md
@@ -1,9 +1,36 @@
-Terraform Custom Roles
+# Terraform Custom Roles
 
-Install Terraform
+## Prerequisites
 
-Run Command 
+1. Gcloud shell access
+2. User running the script must have below permissions:
+orgpolicy.policies.list
+orgpolicy.policy.get
+resourcemanager.projects.get
+resourcemanager.projects.getIamPolicy
+resourcemanager.projects.setIamPolicy
 
-terraform init
+To run the code please follow below commands.
 
-terraform apply
+#### Step-0
+Open cloud shell in GCloud Console
+
+#### Step-1 
+``` $ git clone https://github.com/maheshwari-mohit/terraform-custom-roles.git ```
+
+#### Step-2 
+``` cd terraform-custom-roles ```
+
+#### Step-3 
+Open config.json and fill the required values.
+
+#### Step-4
+``` $ terraform init ```
+
+#### Step-5  => This step needs to be ran only once and when we want to import the role, otherwise skip this step.
+``` $ terraform apply google_project_iam_custom_role.custom_role_import "{your project_id}/{your custom role id}" ``` 
+
+#### Step-6
+``` $ terraform apply --auto-approve ```
+
+Now the custom role with required permission should be created.
--- main.tf
+++ main.tf
@@ -1,26 +1,14 @@
 provider "google" {
-    project = jsondecode(data.local_file.config.content)["project_id"]
 }
-
 
 data "local_file" "config" {
     filename = "${path.module}/config.json"
 }
 
-# Organization custom role
-# resource "google_organization_iam_custom_role" "org_custom_role" {
-#   for_each    = local.platform_roles
-#   role_id     = jsondecode(data.local_file.config.content)["role_id"]
-#   org_id      = ""
-#   title       = ""
-#   description = ""
-#   permissions = []
-# }
 
 # Project custom role
 resource "google_project_iam_custom_role" "project_custom_role" {
-#   for_each = toset(jsondecode(data.local_file.config.content)["roles"])
-  for_each = toset([for role in tolist(jsondecode(data.local_file.config.content)["roles"]) : jsonencode(role)])
+  for_each = toset([for role in tolist(jsondecode(data.local_file.config.content)["new_roles"]) : jsonencode(role)])
 
   project     = jsondecode(data.local_file.config.content)["project_id"]
   role_id     = jsondecode(each.value)["role_id"]
@@ -30,12 +18,13 @@ resource "google_project_iam_custom_role" "project_custom_role" {
   stage       = "ALPHA"
 }
 
-# resource "google_project_iam_custom_role" "custom_role_import" {
-
-#     project = "testing-dlp-terraform"
-#     role_id = "custom_role"
-#     title       = "title"
-#     description = "A description"
-#     permissions = ["storage.buckets.create",
-#                     "storage.buckets.delete","storage.buckets.get"]
-# }
+# Importing custom role
+resource "google_project_iam_custom_role" "custom_role_import" {
+  count       = jsondecode(data.local_file.config.content)["existing_role"] == "yes" ? 1 : 0
+  project     = jsondecode(data.local_file.config.content)["project_id"]
+  role_id     = jsondecode(data.local_file.config.content)["existing_role"]["role_id"]
+  title       = jsondecode(data.local_file.config.content)["existing_role"]["role_id"]
+  description = jsondecode(data.local_file.config.content)["existing_role"]["description"]
+  permissions = jsondecode(data.local_file.config.content)["existing_role"]["permissions"]
+  stage       = "ALPHA"
+}
